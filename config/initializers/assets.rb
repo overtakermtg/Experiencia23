08c4f2413cf2d50954dcdbbf3e77fb2382dc153c
Overtaker <angel.overtaker@gmail.com>
2017-12-16 15:28:57 -0300

    prueba asset_path

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -10,12 +10,15 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 Rails.application.config.assets.paths << Rails.root.join('vendor')
 Rails.application.config.assets.paths << Rails.root.join('vendor', 'plantilla' )
 Rails.application.config.assets.paths << Rails.root.join('vendor', 'plantilla', 'assets' )
+Rails.application.config.assets.paths << Rails.root.join('vendor', 'plantilla', 'assets', 'img')
+Rails.application.config.assets.paths << Rails.root.join('vendor', 'plantilla', 'assets', 'img', 'screenshots')
+
 
 # Rails.application.config.assets.paths << Rails.root.join('vendor', 'plantilla', 'assets', 'ico')
-# Rails.application.config.assets.paths << Rails.root.join('vendor', 'plantilla', 'assets', 'img')
+
 # Rails.application.config.assets.paths << Rails.root.join('vendor', 'plantilla', 'assets', 'img', 'backgrounds')
 # Rails.application.config.assets.paths << Rails.root.join('vendor', 'plantilla', 'assets', 'img', 'modal')
-# Rails.application.config.assets.paths << Rails.root.join('vendor', 'plantilla', 'assets', 'img', 'screenshots')
+
 # Rails.application.config.assets.paths << Rails.root.join('vendor', 'plantilla', 'assets', 'img', 'team')
 # Rails.application.config.assets.paths << Rails.root.join('vendor', 'plantilla', 'assets', 'img', 'testimonials')
 
@@ -24,4 +27,4 @@ Rails.application.config.assets.paths << Rails.root.join('vendor', 'plantilla', 
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
-Rails.application.config.assets.precompile += %w( *.jpg )
+Rails.application.config.assets.precompile += %w( *.jpg *.png )
