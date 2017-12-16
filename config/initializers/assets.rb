c0db17b1e709ffe3ccc39519b71fa00443d4bd46
Overtaker <angel.overtaker@gmail.com>
2017-12-16 11:44:49 -0300

    termine con la simple modificacion del assets

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -7,8 +7,20 @@ Rails.application.config.assets.version = '1.0'
 # Rails.application.config.assets.paths << Emoji.images_path
 # Add Yarn node_modules folder to the asset load path.
 Rails.application.config.assets.paths << Rails.root.join('node_modules')
+Rails.application.config.assets.paths << Rails.root.join('vendor')
+Rails.application.config.assets.paths << Rails.root.join('vendor', 'plantilla' )
+Rails.application.config.assets.paths << Rails.root.join('vendor', 'plantilla', 'assets' )
+# Rails.application.config.assets.paths << Rails.root.join('vendor', 'plantilla', 'assets', 'ico')
+# Rails.application.config.assets.paths << Rails.root.join('vendor', 'plantilla', 'assets', 'img')
+# Rails.application.config.assets.paths << Rails.root.join('vendor', 'plantilla', 'assets', 'img', 'backgrounds')
+# Rails.application.config.assets.paths << Rails.root.join('vendor', 'plantilla', 'assets', 'img', 'modal')
+# Rails.application.config.assets.paths << Rails.root.join('vendor', 'plantilla', 'assets', 'img', 'screenshots')
+# Rails.application.config.assets.paths << Rails.root.join('vendor', 'plantilla', 'assets', 'img', 'team')
+# Rails.application.config.assets.paths << Rails.root.join('vendor', 'plantilla', 'assets', 'img', 'testimonials')
+
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
+Rails.application.config.assets.precompile += %w( *.jpg )
